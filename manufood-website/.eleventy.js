ce02bf053c4c3b34afac7a5c47a4ba068a78cded
nilushan <nilushan.silva@gmail.com>
2025-02-23 07:43:21 +0000

    responsive images

--- manufood-website/.eleventy.js
+++ manufood-website/.eleventy.js
@@ -1,13 +1,37 @@
+const Image = require("@11ty/eleventy-img");
+const path = require("path");
+
 module.exports = function(eleventyConfig) {
   // Add passthrough copy for styles, scripts and images
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/images");
+  // eleventyConfig.addPassthroughCopy("src/images");
 
   // Watch all content files
   eleventyConfig.addWatchTarget("./src/");
   eleventyConfig.addWatchTarget("./src/styles/");
   eleventyConfig.addWatchTarget("./src/js/");
+
+  eleventyConfig.addAsyncShortcode("respImage", async function(src, alt, className) {
+    if (!alt) throw new Error(`Missing alt text for image: ${src}`);
+
+    let metadata = await Image(path.join("./src", src), {
+      widths: [300, 600, 900],
+      formats: ["webp", "jpeg"],
+      outputDir: "./_site/img/",
+      urlPath: "/img/"
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes: "(max-width: 600px) 300px, (max-width: 900px) 600px, 900px",
+      loading: "lazy",
+      decoding: "async",
+      class: className || ""
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  });
 
   // Return the configuration object
   return {
